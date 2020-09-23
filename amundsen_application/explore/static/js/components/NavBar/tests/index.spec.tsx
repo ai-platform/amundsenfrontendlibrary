fab6e7890e2949d29a3de32d49d00273ee4f2359
Andrew Shi <munkyshi22@gmail.com>
2020-09-23 13:44:21 -0700

    Fix broken NavBar tests (or remove no longer relevant ones)

--- amundsen_application/explore/static/js/components/NavBar/tests/index.spec.tsx
+++ amundsen_application/explore/static/js/components/NavBar/tests/index.spec.tsx
@@ -92,7 +92,7 @@ describe('NavBar', () => {
     });
 
     it('returns null if conditions to render search bar are not met', () => {
-      const { props, wrapper } = setup(null, { pathname: "/" });
+      const { props, wrapper } = setup(null, { pathname: "/explore/" });
       expect(wrapper.instance().renderSearchBar()).toBe(null);
     });
   });
@@ -122,7 +122,7 @@ describe('NavBar', () => {
 
     it('renders homepage Link with correct path ', () => {
       element = wrapper.find('#nav-bar-left').find(Link);
-      expect(element.props().to).toEqual('/');
+      expect(element.props().to).toEqual('/explore/');
     });
 
     it('renders homepage Link with correct text', () => {
@@ -137,48 +137,6 @@ describe('NavBar', () => {
     it('calls renderSearchBar', () => {
       expect(renderSearchBarSpy).toHaveBeenCalled();
     });
-
-    it('renders Feedback component', () => {
-      expect(wrapper.find(Feedback).exists()).toBe(true);
-    });
-
-    it('renders Avatar for loggedInUser', () => {
-      expect(wrapper.find(Avatar).props()).toMatchObject({
-        name: props.loggedInUser.display_name,
-        size: 32,
-        round: true,
-      })
-    });
-
-    describe('if indexUsers is enabled', () => {
-      it('renders Avatar for loggedInUser inside of user dropdown', () => {
-        expect(wrapper.find(Dropdown).find(Dropdown.Toggle).find(Avatar).props()).toMatchObject({
-          name: props.loggedInUser.display_name,
-          size: 32,
-          round: true,
-        })
-      });
-
-      it('renders user dropdown header', () => {
-        element = wrapper.find(Dropdown).find(Dropdown.Menu).find('.profile-menu-header');
-        expect(element.children().at(0).text()).toEqual(props.loggedInUser.display_name);
-        expect(element.children().at(1).text()).toEqual(props.loggedInUser.email);
-      });
-
-      it('renders My Profile link correctly inside of user dropdown', () => {
-        element = wrapper.find(Dropdown).find(Dropdown.Menu).find(MenuItem).at(0);
-        expect(element.children().text()).toEqual('My Profile');
-        expect(element.props().to).toEqual('/explore/user/test0?source=navbar');
-      });
-    });
-
-    describe('if indexUsers is disabled', () => {
-      it('does not render a Link to the user profile', () => {
-        AppConfig.indexUsers.enabled = false;
-        const { wrapper } = setup();
-        expect(wrapper.find('#nav-bar-avatar-link').exists()).toBe(false)
-      });
-    })
   });
 });
 
